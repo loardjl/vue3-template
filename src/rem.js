86f1635a52206497d80df5a27bd25d4770efc570
chunlaizhang <chunlai.zhang@ujoin-tech.com>
2023-03-23 14:12:48 +0800

    init

---
+++ src/rem.js
@@ -0,0 +1,14 @@
+// 设置 rem 函数
+function setRem () {
+  // 320 默认大小16px; 320px = 20rem ;每个元素px基础上/16
+  let htmlWidth = document.documentElement.clientWidth || document.body.clientWidth;
+  //得到html的Dom元素
+  let htmlDom = document.getElementsByTagName('html')[0];
+  //设置根元素字体大小
+  htmlDom.style.fontSize= htmlWidth/(1920/16) + 'px';
+}
+setRem();
+// 改变窗口大小时重新设置 rem
+window.addEventListener('resize', function () {
+  setRem()
+})
---
+++ src/views/AboutView.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="about">
+    <h1 class="count">{{ count }}</h1>
+    <h1>{{ doubleCount }}</h1>
+    <el-button type="primary" @click="increment">click</el-button>
+  </div>
+</template>
+<script setup>
+import { useCounterStore } from '@/stores/counter'
+import { storeToRefs } from 'pinia'
+import { onMounted } from 'vue'
+const store = useCounterStore()
+const { count, doubleCount } = storeToRefs(store)
+const { increment } = store
+onMounted(() => {
+  console.log(count)
+})
+const func = value =>{
+  console.log('func')
+}
+</script>
+<style scoped lang="scss">
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+    font-size: 14px;
+
+    .count {
+      font-weight: 700;
+    }
+  }
+}
+</style>
